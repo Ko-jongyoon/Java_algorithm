ce919d6d6cb7bd5a708c4e51baa22effb3f57680
Ko-jongyoon <rhwhddbs12@naver.com>
2019-02-17 16:03:41 +0900

    practice

--- helloworld/Main.java
+++ helloworld/Main.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class Main{
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-    
-        int A =scan.nextInt();
-        int B =scan.nextInt();
-       
-        System.out.println(A+B);
+        String a = scan.nextLine();
+        System.out.println(a);
+
     }
-}   
+}
